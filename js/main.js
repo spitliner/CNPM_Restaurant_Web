d2132de55dc11dd2d08800fa7b6d89a650c43d20
Akihito Shu <58225688+doantaydo@users.noreply.github.com>
2021-11-14 00:01:13 +0700

    Shu commit
    Shu add footer edited.

--- js/main.js
+++ js/main.js
@@ -116,7 +116,7 @@ function updatecart() {
     var quantity = quantity_item.value
       total = total + (price * quantity)
   }
-  document.getElementsByClassName("cart-total-price")[0].innerText = total + 'VNĐ'
+  document.getElementsByClassName("cart-total-price")[0].innerText = total + '$'
 }
 
 // menu mobile
@@ -129,3 +129,23 @@ btn_menu.addEventListener("click", function () {
     item_menu.style.display = "block";
   }
 })
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
