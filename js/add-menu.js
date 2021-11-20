5167368bf68fcbdffd3d5928a0834b5a949f2d75
Akihito Shu <58225688+doantaydo@users.noreply.github.com>
2021-11-21 00:02:16 +0700

    Shu commit.
    Add final php and js for dynamic menu.

--- js/add-menu.js
+++ js/add-menu.js
@@ -11,44 +11,63 @@ for (var i = 0; i < category_btn.length; i++) {
     })
 }
 function loadMenu(category) {
-    // var menu = chỗ này đặt code trả về menu theo category
-    fetch("http://127.0.0.1:5500/php/connectDB.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        },
-        body: `cate=${category}`,
-    })
-        .then((response) => response.text())
-        .then((res) => (document.getElementById("result").innerHTML = res));
-    // var menu = <?php echo getDB(category)?>;
-        // thêm item mới trong menu ở đây
-    return
-        // dưới đây là thêm phần mở ra
-//         var mini_zone = document.getElementsByClassName("mini-zone");
-//         var mini_zone_content = `
-// <div id="${item.name}" class="modal">
-//     <div class="mini-zone-content">
-//         <div class="mini-zone-header">
-//             <h5 class="mini-zone-modal-title">${item.name}</h5>
-//             <span class="mini-zone-close">&times;</span>
-//         </div>
-//         <div class="mini-zone-body">
-//             <div class="container">
-//                 <div class="row">
-//                     <div class="col-sm-6">
-//                         <img class="img-prd" style="width: 100%;border-radius: 30px" src="${item.img}" alt="">
-//                     </div>
-//                     <div class="col-sm-6">
-//                         <span class="mini-zone-description">${item.description}</span>
-//                         <span class="mini-zone-price">Price:<span class="mini-zone-money">${item.price}</span></span>
-//                         <button type="button" class="btn mini-zone-btn-cart">Thêm Vào Giỏ</button>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-// </div>
-//         `;
-//         mini_zone.append(mini_zone_content);
+    //alert(category)
+    const dbParam = JSON.stringify({ "id_cate": category });
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function () {
+        const myObj = JSON.parse(this.responseText);
+        let menu = "";
+        let mini = "";
+        let name_a = "";
+        for (let x in myObj) {
+            name_a += myObj[x].name_item + " ";
+            menu += `
+<div class="main - product">
+    <div class="open-mini-zone" >
+        <div class="img-product">
+            <img class="img-prd" src="` + myObj[x].img + `" alt="">
+        </div>
+        <div class="content-product">
+            <h3 class="content-product-h3">`+ myObj[x].name_item +`</h3>
+            <span class="content-product-des">`+ myObj[x].name_eng +`</span>
+        </div>
+    </div>
+    <div class="content-product-deltals">
+        <span class="price">`+ myObj[x].price +`</span>
+        <button type="button" class="btn btn-cart">Thêm Vào Giỏ</button>
+    </div>
+</div>
+            `;
+            mini += `
+        <div id="`+ myObj[x].name_item +`" class="modal">
+            <div class="mini-zone-content">
+                <div class="mini-zone-header">
+                    <h5 class="mini-zone-modal-title">`+ myObj[x].name_item +`</h5>
+                    <span class="mini-zone-close">&times;</span>
+                </div>
+                <div class="mini-zone-body">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <img class="img-prd" style="width: 100%;border-radius: 30px" src="` + myObj[x].img + `" alt="">
+                            </div>
+                            <div class="col-sm-6">
+                                <span class="mini-zone-description">` + myObj[x].desc_item + `</span>
+                                <span class="mini-zone-price">Price:<span class="mini-zone-money">`+ myObj[x].price +`</span></span>
+                                <button type="button" class="btn mini-zone-btn-cart">Thêm Vào Giỏ</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            `;
+        }
+        document.getElementsByClassName("products")[0].innerHTML = menu;
+        document.getElementsByClassName("mini-zone")[0].innerHTML = mini;
+        alert(name_a)
+    }
+    xmlhttp.open("POST", "php/connectDB.php");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("x=" + dbParam);
 }
--- php/connectDB.php
+++ php/connectDB.php
@@ -1,54 +1,20 @@
 <?php
-$servername = "127.0.0.1:5500";
+$servername = "localhost:8000";
 $database = "pos_system";
 $username = "cnpm_web";
 $password = "Sx6tH_Z96JnB7z_cjbuJJg=G7cmQ8_-t";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-$cate = $_POST['cate'];
-function fetch_data($cate){
-    global $db;
-        $query="
-            SELECT `name`,`name-eng`,`img`,`price`,`description`
-            FROM `menu`, `category_food`
-            WHERE  `menu`.`id` = `category_food`.`id` AND `category_food`.`id_cate`=$cate";
-        $exec=mysqli_query($db, $query);
-        if(mysqli_num_rows($exec)>0){
-            $row= mysqli_fetch_all($exec, MYSQLI_ASSOC);
-            return $row;  
-        }else{
-            return $row=[];
-        }
-}
-$fetchData= fetch_data($cate);
-getMenu($fetchData);
-function getMenu($fetchData) {
-  foreach($fetchData as $data) {
-    echo '
-<div class="main-product" >
-    <div class="open-mini-zone">
-        <div class="img-product">
-            <img class="img-prd" src="'.$data[`img`].'" alt="">
-        </div>
-        <div class="content-product">
-            <h3 class="content-product-h3">'.$data[`name`].'</h3>
-            <span class="content-product-des">'.$data[`name-eng`].'</span>
-        </div>
-    </div>
-    <div class="content-product-deltals">
-        <span class="price">'.$data[`price`].'</span>
-        <button type="button" class="btn btn-cart">Thêm Vào Giỏ</button>
-    </div>
-</div > 
-    '
-  }
-}
-// function getMiniZone($cate) {
+header("Content-Type: application/json; charset=UTF-8");
+$obj = json_decode($_POST["x"], false);
 
-// }
+$conn = new mysqli($servername, $username, $password, $dbname);
+$stmt = $conn->prepare("SELECT `name` as name_item,`name-eng` as name_eng,`img` as img,`price` as price,`description` as desc_item
+                        FROM `menu`, `category_food`
+                        WHERE  `menu`.`id` = `category_food`.`id` AND `category_food`.`id_cate`= ?");
+$stmt->bind_param("s", $obj->limit);
+$stmt->execute();
+$result = $stmt->get_result();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($outp);
 ?>
